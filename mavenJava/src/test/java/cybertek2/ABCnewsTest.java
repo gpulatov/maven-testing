2bfc8e5488a89d24cb20abc5fd368014220ef04b
gpulatov <gpulatov@gmail.com>
2018-12-10 23:30:52 -0600

    maven-Java

---
+++ mavenJava/src/test/java/cybertek2/ABCnewsTest.java
@@ -0,0 +1,20 @@
+package cybertek2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ABCnewsTest {
+	
+	@Test
+	public void navigateToABCnews() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://abc.go.com/");
+		driver.quit();
+	}
+
+}
