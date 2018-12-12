b0e43ff6d67135f0217f799d3f2847f4c63308b4
gpulatov <gpulatov@gmail.com>
2018-12-12 15:20:28 -0600

    test using Grid

---
+++ selenium-maven-grid/src/test/java/com/grid/gridTest2.java
@@ -0,0 +1,24 @@
+package com.grid;
+
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class gridTest2 {
+	
+	@Test
+	public void login() throws MalformedURLException {
+		DesiredCapabilities caps = DesiredCapabilities.chrome();
+		caps.setPlatform(Platform.ANY);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.68:4444/wd/hub"), caps);
+		driver.get("https://www.cnn.com/");
+		
+	}
+	
+	
+}
